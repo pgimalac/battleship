6eca889ee9a2c9ad4d2f5172238ca01fdf2deec5
Pierre Gimalac <pierre.gimalac@gmail.com>
2019-01-29 01:45:51 +0100

    create game, boat, player & direction files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+mod boat;
+mod direction;
+mod game;
+mod player;
+
+use crate::boat::Boat;
+use crate::boat::Class;
+use crate::direction::Direction;
+use crate::game::Game;
+use crate::player::Player;
+use std::rc::Rc;
+
 fn main() {
-    println!("Hello, world!");
+    let carrier = Boat::new(Class::Carrier, (0, 0), Direction::Right);
+    let battleship = Boat::new(Class::Battleship, (0, 1), Direction::Up);
+    let cruiser = Boat::new(Class::Cruiser, (11, 1), Direction::Left);
+    let submarine = Boat::new(Class::Submarine, (11, 11), Direction::Down);
+    let destroyer = Boat::new(Class::Destroyer, (5, 5), Direction::Left);
+
+    let player = Player::new(
+        String::from("Pierre"),
+        vec![
+            Rc::new(carrier),
+            Rc::new(battleship),
+            Rc::new(cruiser),
+            Rc::new(submarine),
+            Rc::new(destroyer),
+        ],
+    );
+
+    let game = Game::new(12, 12, player);
+    println!("{:#?}", game);
 }
